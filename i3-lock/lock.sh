3fe10a5ba91f1429c40ec0099e45ef70c5b256a0
Edoardo Rosa <edoardo.rosa90@gmail.com>
2021-09-26 18:30:12 +0200

    fix firefox

--- i3-lock/lock.sh
+++ i3-lock/lock.sh
@@ -9,21 +9,22 @@ for RES in ${SR}; do
   rectangles+="rectangle $CX,$CY $((CX+250)),$((CY-80)) "
 done
 
-TMPBG=/tmp/screen.png
+TMPBG=/tmp/.screen.png
 maim $TMPBG && convert $TMPBG -scale 5% -scale 2000% -draw "fill black fill-opacity 0.4 $rectangles" $TMPBG
 
 i3lock \
   -i $TMPBG \
-  --time-pos="x+90:h-101" \
-  --date-pos="tx-0:ty+25" \
+  --time-pos="x+100:h-100" \
+  --date-pos="x+110:h-80" \
   --clock --date-str="%A %d %B" \
   --line-uses-inside \
-  --keyhl-color=d23c3dff --bshl-color=d23c3dff --separator-color=00000000 \
-  --ind-pos="x+190:h-99" \
-  --radius=20 --ring-width=3 --verif-text="" --wrong-text="" \
+  --keyhl-color="1976D2FF" --bshl-color=d23c3dff --separator-color=00000000 \
+  --ind-pos="x+225:h-102" \
+  --radius=22 --ring-width=3 --verif-text="" --wrong-text="" --verif-size=50 --wrong-size=50  \
   --color="ffffffff" --time-color="ffffffff" --date-color="ffffffff" \
   --inside-color="00000000" --ring-color="ffffffff" \
-  --insidever-color="fecf4dff" --insidewrong-color="d23c3dff" \
-  --ringver-color="fecf4dff" --ringwrong-color="d23c3dff"
+  --insidever-color="FFEB3BFF" --insidewrong-color="d23c3dff" \
+  --ringver-color="FFEB3BFF" --ringwrong-color="d23c3dff" \
+  --noinput-text "" --pass-media-keys --pass-volume-keys
 
 rm $TMPBG
