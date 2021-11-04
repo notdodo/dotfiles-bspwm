4f9657bb5fefcf8a90debb3075b1499ef774977e
Edoardo Rosa <edoardo.rosa90@gmail.com>
2021-11-04 07:53:42 +0100

    updates

--- polybar/scripts/volume.sh
+++ polybar/scripts/volume.sh
@@ -2,7 +2,6 @@
 
 function main() {
     DEFAULT_SINK_ID=$(pactl info |rg -o 'Default Sink: (.*)' -r '$1')
-    VOLUME="$(pactl list sinks | rg -i ${DEFAULT_SINK_ID} | rg '^[[:space:]]Volume:' | head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')"
     VOLUME="$(pactl list sinks|rg -i ${DEFAULT_SINK_ID} -A 7 | rg -io '(\d{0,3})%' -r '$1' | head -n 1)"
     IS_MUTED="$(pactl list sinks|rg -i ${DEFAULT_SINK_ID} -A 7 | rg "Mute: yes")"
 
